72e9b2621a9ff898bfcb8f8b696e6acd8b250ab3
KimHayoon <ekgp6360@naver.com>
2020-02-26 17:26:36 +0900

    Update Client.cpp

--- DirectX12/01.Project/Client/Client.cpp
+++ DirectX12/01.Project/Client/Client.cpp
@@ -12,6 +12,7 @@
 #include "LoginScene.h"
 #include "IngameScene.h"
 #include "ChatScript.h"
+#include "IngameScene.h"
 
 #define MAX_LOADSTRING 100
 
@@ -53,6 +54,8 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 
 	CScene* pScene = CSceneMgr::GetInst()->GetCurScene();
 	CIngameScene* pLoginScene = pScene->CreateSceneScript<CIngameScene>( L"LoginScene" );
+	CLoginScene* pLoginScene = pScene->CreateSceneScript<CLoginScene>( L"LoginScene" );
+	//CIngameScene* pGameScene = pScene->CreateSceneScript<CIngameScene>(L"GameScene");
 
 	HACCEL hAccelTable = LoadAccelerators( hInstance, MAKEINTRESOURCE( IDC_CLIENT ) );
 	MSG msg;
