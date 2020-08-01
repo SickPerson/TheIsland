8ef926a9a731600c2111d09fd92a49ee757ad301
Lee Dong Gyu <crow4079@naver.com>
2020-08-01 16:42:22 +0900

    Add Housing Resource

--- DirectX12/01.Project/Client/HousingMgr.cpp
+++ DirectX12/01.Project/Client/HousingMgr.cpp
@@ -16,6 +16,9 @@ CHousingMgr::~CHousingMgr()
 
 void CHousingMgr::Init()
 {
+	//Ptr<CMeshData> pTestMeshData = CResMgr::GetInst()->LoadFBX(L"FBX\\housing_door_stone.fbx", 1);
+	//pTestMeshData->Save(pTestMeshData->GetPath());
+
 	for (int i = HOUSING_FOUNDATION; i < HOUSING_END; ++i)
 	{
 		switch ((HOUSING_TYPE)i)
@@ -23,26 +26,31 @@ void CHousingMgr::Init()
 		case HOUSING_FOUNDATION:
 		{
 			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_foundation.mdat", L"MeshData\\housing_foundation.mdat");
+			m_pTex[i][1] = CResMgr::GetInst()->Load<CMeshData>(L"housing_foundation.mdat", L"MeshData\\housing_foundation.mdat");
 		}
 			break;
 		case HOUSING_WALL:
 		{
-			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_wall.mdat", L"MeshData\\housing_wall.mdat");
+			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_wall_stone.mdat", L"MeshData\\housing_wall_stone.mdat");
+			m_pTex[i][1] = CResMgr::GetInst()->Load<CMeshData>(L"housing_wall.mdat", L"MeshData\\housing_wall.mdat");
 		}
 			break;
 		case HOUSING_DOOR:
 		{
-			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_door.mdat", L"MeshData\\housing_door.mdat");
+			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_door_stone.mdat", L"MeshData\\housing_door_stone.mdat");
+			m_pTex[i][1] = CResMgr::GetInst()->Load<CMeshData>(L"housing_door.mdat", L"MeshData\\housing_door.mdat");
 		}
 			break;
 		case HOUSING_WINDOW:
 		{
-			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_window.mdat", L"MeshData\\housing_window.mdat");
+			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_window_stone.mdat", L"MeshData\\housing_window_stone.mdat");
+			m_pTex[i][1] = CResMgr::GetInst()->Load<CMeshData>(L"housing_window.mdat", L"MeshData\\housing_window.mdat");
 		}
 			break;
 		case HOUSING_FLOOR:
 		{
-			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_floor.mdat", L"MeshData\\housing_floor.mdat");
+			m_pTex[i][0] = CResMgr::GetInst()->Load<CMeshData>(L"housing_floor_stone.mdat", L"MeshData\\housing_floor_stone.mdat");
+			m_pTex[i][1] = CResMgr::GetInst()->Load<CMeshData>(L"housing_floor.mdat", L"MeshData\\housing_floor.mdat");
 		}
 			break;
 		default:
