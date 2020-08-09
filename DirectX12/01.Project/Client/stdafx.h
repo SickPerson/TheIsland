eaa10e84562b934930a5b95c8cd456bd3f1dc042
Lee Dong Gyu <crow4079@naver.com>
2020-08-10 06:29:35 +0900

    Ready Presentation

--- DirectX12/01.Project/Client/stdafx.h
+++ DirectX12/01.Project/Client/stdafx.h
@@ -7,11 +7,11 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "targetver.h"
 
-#ifdef UNICODE
-#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
-#else
-#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
-#endif
+//#ifdef UNICODE
+//#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+//#else
+//#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+//#endif
 
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
